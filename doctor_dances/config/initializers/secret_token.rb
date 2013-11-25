7a92346ab0f96e7bbd6f713c9c5ba111936606f4
Eben <eben.lowe@gmail.com>
2013-11-25 10:58:49 -0500

    Rails new.

---
+++ doctor_dances/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoctorDances::Application.config.secret_key_base = '17a5f5197aa8c476cea83cc2f0c85216ac8135e6f251373cb190510e54f725da41267dcb9ea77b59788472a6ce4c4224988c8720a05788c5911a35cbe2765777'
---
+++ doctor_dances/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DoctorDances::Application.config.session_store :cookie_store, key: '_doctor_dances_session'
